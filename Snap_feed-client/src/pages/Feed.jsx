a61c73f12ffd3b1e9826f9dcc7eff38b15c1ec92
Anand-1081 <jha913003@gmail.com>
2026-06-22 14:48:33 +0530

    fix: add default route

--- Snap_feed-client/src/pages/Feed.jsx
+++ Snap_feed-client/src/pages/Feed.jsx
@@ -1,7 +1,7 @@
 import React, { useState,useEffect } from 'react'
 import axios from "axios"
 import { useNavigate } from "react-router-dom"
-import CreatePost from './pages/CreatePost.jsx'
+import CreatePost from './CreatePost.jsx'
 
 
 
