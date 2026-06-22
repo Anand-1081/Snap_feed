a4fa0280697da0eeb1db4b6e153cd65ca58b4d19
Anand-1081 <jha913003@gmail.com>
2026-06-22 14:37:48 +0530

    fix: add default route

--- Snap_feed-client/src/pages/Feed.jsx
+++ Snap_feed-client/src/pages/Feed.jsx
@@ -1,5 +1,6 @@
 import React, { useState,useEffect } from 'react'
 import axios from "axios"
+import { useNavigate } from "react-router-dom"
 
 
 const Feed = () => {
@@ -21,6 +22,10 @@ const Feed = () => {
     return (
 
         <section className='feed-section' >
+           
+            <button onClick={() => navigate("/create-post")}>
+                + Create Post
+            </button> 
 
             {
                 posts.length > 0 ? (
